e5e82de654668146cc45db8d35003f0d49b8e472
Lakshay <imlakshaykumar@gmail.com>
2025-08-15 01:36:55 +0530

    feat: added mmkv storage

---
+++ lib/store/storage.ts
@@ -0,0 +1,21 @@
+import { MMKV } from 'react-native-mmkv';
+import { StateStorage } from 'zustand/middleware';
+
+export const storage = new MMKV();
+
+export const mmkvStorage: StateStorage = {
+    setItem: (item: string, value: string) => {
+        storage.set(item, value);
+    },
+    getItem: (item: string) => {
+        const value = storage.getString(item);
+        return value || null;
+    },
+    removeItem: (item: string) => {
+        storage.delete(item);
+    },
+}
+
+export const storageKeys = {
+
+} as const;
